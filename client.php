0713a82b28096f4451642aba4b1cd244bb45a9af
LowLevelSubmarine <33373745+LowLevelSubmarine@users.noreply.github.com>
2018-07-05 15:43:41 +0200

    Made ui look better

--- client.php
+++ client.php
@@ -2,11 +2,15 @@
 	include "settings.php";
 ?>
 <head>
+    <link type="text/css" rel="stylesheet" href="style.css">
 	<title><?php echo name ?></title>
 </head>
 <body>
+    <h1>
+        <?php echo name ?>
+    </h1>
 	<form action="client-code.php" method="POST">
-		<input type="text" name="nummer"><br>
-		<input type="submit" value="Ok">
+		<input class="fill" type="text" name="nummer"><br>
+		<input class="button" type="submit" value="Ok">
 	</form>
 </body>
