fdf31945e6197f366911d1aaab52b76d09ab3fd0
Jonbeckas <32589685+Jonbeckas@users.noreply.github.com>
2018-08-19 17:33:02 +0200

    zum letzten mal

--- settings.php
+++ settings.php
@@ -8,7 +8,7 @@
 
 	//ANDERES
 	define("name","ISPOLASO",true);
-	define("url","localhost",true);
+	define("url","",true);
 	define("mintime",30,true);	//in sekunden
 	define("countdown",1000,true); // in sekunden*1000 ALSO in millisekunden
 	define("maxschueler",850,true);
