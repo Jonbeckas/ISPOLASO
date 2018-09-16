b910f27dd2c56e5157d760ce2ab8a12f420d9964
Jonbeckas <32589685+Jonbeckas@users.noreply.github.com>
2018-09-16 19:09:56 +0200

    1.8

--- settings.php
+++ settings.php
@@ -1,17 +1,17 @@
 <?php
 	//MYSQL DATEN
-	define("host","localhost",true);
-	define("password","jonibeck1",true);
-	define("user","root",true);
-	define("database","barcode",true);
-	define("table","schueler",true);
+	define("host","",true);
+	define("password","",true);
+	define("user","",true);
+	define("database","",true);
+	define("table","",true);
 
 	//ANDERES
-	define("name","ISPOLASO",true);
-	define("spruch","Gymnasium Vohwinkel Sponsorenlauf 2018",true);
-	define("url","",true);
-	define("mintime",30,true);	//in sekunden
+	define("name","NAME DER INSTALATION",true);
+	define("spruch","MOTTO",true);
+	define("url","",true);//freilassen wenn im rootverzeichniss sonst: /irgendwas/Beispiel für domain.beispiel/irgendwas/Beispiel
+	define("mintime",ZEIT DIE EIN SCHÜLER min. GERANNT SEIN MUSS,true);	//in sekunden
 	define("countdown",1000,true); // in sekunden*1000 ALSO in millisekunden
-	define("minttime",60,true);
-	define("vermisttime",2700,true); //in sekunden
+	define("minttime",0,true); //Mindest Anwesenheit  Zeit
+	define("vermisttime",AB WANN MAN VERMISST WIRD,true); //in sekunden
 ?>
