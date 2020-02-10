f5e4c33be77065a1d50c9328398c70b496617354
Jonbeckas <Jonasbeckerx3@outlook.de>
2020-02-10 18:29:06 +0100

    correct some formating issues

--- manager.php
+++ manager.php
@@ -793,9 +793,9 @@
 									<a id=Fehlermeldung>ISPOLASO Version 1.8</a><br></p>
 									<a id=Zentrieren rel=\"license\"><p>Copyright 2019 Marten Schiwek und Jonas Becker<br>
 
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at<br>
+Licensed under the Apache License, Version 2.0 (the \"License\"); you may not use this file except in compliance with the License. You may obtain a copy of the License at<br>
 
-http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.</p>
+http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.</p>
 								</div>
 							</body>";
 			}
@@ -835,9 +835,9 @@ http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or 
 									<a id=Fehlermeldung>ISPOLASO Version 1.8</a><br></p>
 								<p>	Copyright 2019 Marten Schiwek und Jonas Becker
 
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the \"License\"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.</p>
+http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.</p>
 								</div>
 							</body>";
 			}
